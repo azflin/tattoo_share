de791be38e9810048d15e04018dfc297ba83c3ea
Allen Lin <allenlin1992@hotmail.com>
2020-09-22 19:51:56 -0400

    Create Tattoo base case

---
+++ front-end/src/app/tattoo.ts
@@ -0,0 +1,5 @@
+export interface Tattoo {
+  id: number;
+  description: string;
+  photo: string;
+}
--- front-end/src/app/tattoos/tattoos.component.ts
+++ front-end/src/app/tattoos/tattoos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tattoo } from "../tattoo";
 
 @Component({
   selector: 'app-tattoos',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tattoos.component.css']
 })
 export class TattoosComponent implements OnInit {
-
-  description = "Raijin sleeve";
+  tattoo: Tattoo = {
+    id: 1,
+    description: 'Raijin sleeve',
+    photo: 'http://127.0.0.1:8000/media/tattoos/raijin_sleeve.jpg'
+  }
 
   constructor() { }
 
