b5d711d6f28646b91c31636fb7c0fc16009e5ead
Allen Lin <allenlin1992@hotmail.com>
2020-09-27 22:54:26 -0400

    Create django admin for Tattoo

--- tattoo_share/api/models.py
+++ tattoo_share/api/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Tattoo(models.Model):
     description = models.TextField()
     photo = models.ImageField(upload_to='tattoos')
+
+    def __str__(self):
+        return "<Tattoo(id={}, description={})>".format(self.id, self.description)
