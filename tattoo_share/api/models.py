5f86294cbe910bb6b7514ab3908de5a02699148d
Allen Lin <allenlin1992@hotmail.com>
2020-10-14 19:20:33 -0400

    Add artist field

--- tattoo_share/api/models.py
+++ tattoo_share/api/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Tattoo(models.Model):
-    description = models.TextField()
+    description = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='tattoos')
+    artist = models.CharField(max_length=200)
 
     def __str__(self):
         return "<Tattoo(id={}, description={})>".format(self.id, self.description)
